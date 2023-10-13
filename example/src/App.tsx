2baedd359fb34cf4d6ee046c20982d2e1fc9d581
Zikun Wang <68682076+zikunw@users.noreply.github.com>
2023-10-12 21:45:57 -0400

    create example for demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,8 +5,16 @@ import { compiler, renderGraph, Graph, GraphType } from 'cavalcader';
 import useWindowDimensions from './hook/useWindowDimension';
 import { FaGithubSquare } from 'react-icons/fa';
 
+const exampleInput = `LR:
+(source) => <operator1> => [sink1];
+(source) => <operator2> => [sink2];
+(source) => <operator3> => [sink2];
+[sink1] => (database);
+[sink2] => (database);
+`
+
 function App() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(exampleInput);
   const output = compiler(input);
   const { width } = useWindowDimensions();
   const renderResult = "message" in output ?  renderGraph(new Graph(GraphType.LeftRight), width-80) : renderGraph(output, width-80);
@@ -14,7 +22,7 @@ function App() {
     <>
       <div className="w-full h-full p-10">
         <h1 className="text-2xl font-bold flex flex-row">Demo  <a href="https://github.com/zikunw/Cavalcader.js"><FaGithubSquare /></a></h1>
-        <textarea value={input} onChange={e => setInput(e.target.value)} className='border-2 w-full my-4 p-2' placeholder='LR: (source) => [sink]'/>
+        <textarea value={input} onChange={e => setInput(e.target.value)} className='border-2 w-full my-4 p-2 h-60' placeholder='LR: (source) => [sink]'/>
         {renderResult}
         <p className="text-sm text-gray-400 mt-5">Made by Zikun Wang.</p>
       </div>
